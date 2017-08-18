ade2820f0204454a951482cea9fbf908fe42678e
Aatish Rajkarnikar <aatishrajkarnikar@Amnil-Technologies.local>
2017-08-18 16:06:15 +0545

    view product

--- 360ProductView/ViewController.swift
+++ 360ProductView/ViewController.swift
@@ -10,16 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var imageView: UIImageView!
+    var startIndex = 2
+    var endIndex = 65
+    var currentIndex = 2{
+        didSet{
+            imageView.image = UIImage(named: "\(currentIndex)")
+        }
     }
+    var lastPoint = CGPoint.zero
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func gesture(_ sender: UIPanGestureRecognizer) {
+        
+        if sender.state == .began{
+            lastPoint = sender.location(in: imageView)
+        }else if sender.state == .changed {
+            let velocity = sender.velocity(in: imageView)
+            if velocity.x > 0 {
+                if (sender.location(in: imageView).x > lastPoint.x + 5){
+                    lastPoint = sender.location(in: imageView)
+                    if currentIndex > startIndex{
+                        currentIndex -= 1
+                    }else{
+                        currentIndex = endIndex
+                    }
+                }
+            }else{
+                if (sender.location(in: imageView).x < lastPoint.x - 5){
+                    lastPoint = sender.location(in: imageView)
+                    if currentIndex < endIndex{
+                        currentIndex += 1
+                    }else{
+                        currentIndex = startIndex
+                    }
+                }
+            }
+        }
     }
-
 
 }
 
